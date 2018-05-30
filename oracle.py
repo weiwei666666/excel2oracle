05b1884193d3657ab6dd0b73fbb867cb302147dd
weiwei <244096589@qq.com>
2018-05-30 16:09:05 +0800

    20180530
    First successful trial,this version can draw a bar graph for only 1 day and put the data to the database at the same time.

--- oracle.py
+++ oracle.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-#coding=utf-8
-
 import cx_Oracle as oracle
 import xlrd as reader
 import os
@@ -8,14 +5,17 @@ import time
 import numpy as np
 from pandas import Series,DataFrame
 import matplotlib.pyplot as plt
+import io
+import sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 
 cur_time = time.strftime("%Y%m%d",time.localtime())
 file_path = "D:\\Excel"
-table_name= "TR_STAT_"+cur_time
-insert_query = "INSERT INTO " + table_name + " (COD_TR,NAM_TR,NUM_TR,DATE_TR) VALUES (%s,%s,%d,%s)"
+table_name= "TR_STAT_CH"
+insert_query = "INSERT INTO " + table_name + " (COD_TR,NAM_TR,SUM_TR,DATE_TR) VALUES ('%s','%s','%d','%s')"
 count_gap = [10,10000,50000,100000,200000,400000,600000,800000,1000000]
 count_list = [0,0,0,0,0,0,0,0,0,0]
-horizon = ['0-10','10-10000','10000-50000','50000-100000','100000-200000','200000-400000','400000-600000','600000-800000','800000-1000000','1000000~']
+horizon = ['0-10','10-1W','1W-5W','5W-10W','10W-20W','20W-40W','40W-60W','60W-80W','80W-100W','100W~']
 
 def get_base(cursor):
     sql = "select COD_TR from TR_NAM_CH"
@@ -40,36 +40,45 @@ def count_num(total):
     return
 
 def draw_bar_graph():
-    df = DataFrame(count_list,columns=['交易量'],index=horizon)
+    df = DataFrame(count_list,columns=['volume of transaction'],index=horizon)
     df.plot(kind='bar')
     plt.show()
 
+def type_transfer(base_sql):
+    data = []
+    for i in range(len(base_sql)):
+        data.append(list(base_sql[i])[0])
+    return data
+
 if __name__=='__main__':
-    ipaddr=""
-    username=""
-    password=""
-    oracle_port=""
-    oracle_service=""
+    ipaddr="10.230.171.180"
+    username="avadbo"
+    password="avadbop"
+    oracle_port="1521"
+    oracle_service="ora12c"
     try:
-        connection = oracle.connect(username+"/"+password+"@"+ipaddr+":"+oracle_port+"/"+oracle_service)
+        connection = oracle.connect(username+"/"+password+"@"+ipaddr+":"+oracle_port+"/"+oracle_service,encoding="UTF-8",nencoding="UTF-8")
     except Exception as e:
         print(e)
     else:
         cursor = connection.cursor()
-        base = oraclesql(cursor)
+        base_sql = get_base(cursor)
+        base = type_transfer(base_sql)
+        #print(len(base))
+        #print(isinstance(base,list))
 
     xls_name = get_sheetname()
     xls_data = reader.open_workbook(file_path+"\\"+xls_name)
     #sheetname = data.sheet_names()
-    sheet = xls_data.sheet_by_name('BoEing交易量按渠道统计')
+    sheet = xls_data.sheet_by_name('BoEing交易量按渠道通道统计')
     row_num = sheet.nrows
     col_num = sheet.ncols
-    for i in range(row_num):
+    for i in range(3,row_num):
         row = sheet.row_values(i)
-        if row(i)[0][0:8] in base :
-            cod_tr_value = row(i)[0]
-            nam_tr_value = row(i)[1]
-            num_tr_value = row(i)[4]
+        if row[0] in base :
+            cod_tr_value = row[0]
+            nam_tr_value = row[1]
+            num_tr_value = int(row[4])
             date_tr_value = cur_time
             cursor.execute(insert_query%(cod_tr_value,nam_tr_value,num_tr_value,date_tr_value))
             count_num(num_tr_value)
